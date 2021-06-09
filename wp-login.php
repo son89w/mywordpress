1de5143e69e50dd6be6f0205043177f05749444a
son89w <wildrichair123@gmail.com>
2021-06-09 22:02:06 +0700

    web

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,11 @@
 	<title>Log In &lsaquo; My Wordpress &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://son89w.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://son89w.github.io/mywordpress/wp-includes/css/buttons.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://son89w.github.io/mywordpress/wp-admin/css/forms.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://son89w.github.io/mywordpress/wp-admin/css/l10n.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="login-css" href="https://son89w.github.io/mywordpress/wp-admin/css/login.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://son89w.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=5.7.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://son89w.github.io/mywordpress/wp-includes/css/buttons.min.css?ver=5.7.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://son89w.github.io/mywordpress/wp-admin/css/forms.min.css?ver=5.7.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://son89w.github.io/mywordpress/wp-admin/css/l10n.min.css?ver=5.7.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://son89w.github.io/mywordpress/wp-admin/css/login.min.css?ver=5.7.2" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -53,51 +53,56 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
 			<a href="https://son89w.github.io/mywordpress/">&larr; Go to My Wordpress</a>		</p>
 			</div>
-	<script src="https://son89w.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">
+	<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
 var _zxcvbnSettings = {"src":"https:\/\/son89w.github.io\/mywordpress\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
 </script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
-<script id="wp-polyfill-js-after">
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script type="text/javascript" id="wp-polyfill-js-after">
 ( 'fetch' in window ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
 </script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
-<script id="wp-i18n-js-after">
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script id="password-strength-meter-js-extra">
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
 </script>
-<script id="password-strength-meter-js-translations">
+<script type="text/javascript" id="password-strength-meter-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script src="https://son89w.github.io/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
-<script id="wp-util-js-extra">
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
 </script>
-<script src="https://son89w.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
-<script id="user-profile-js-extra">
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
 var userProfileL10n = {"user_id":"0","nonce":"0e765ec1c8"};
+/* ]]> */
 </script>
-<script id="user-profile-js-translations">
+<script type="text/javascript" id="user-profile-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script src="https://son89w.github.io/mywordpress/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
-	<script>
-	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
-	</script>
-		<div class="clear"></div>
+<script type="text/javascript" src="https://son89w.github.io/mywordpress/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
+	<div class="clear"></div>
 	</body>
 	</html>
